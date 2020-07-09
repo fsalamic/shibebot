fcc85ce1deff5f34f2b68e831162052f57a9cd9a
Griffin <bluedogerino@gmail.com>
2020-07-09 02:16:19 +0200

    add cats and birds commands, and remove unneeded img line

--- shibebot.py
+++ shibebot.py
@@ -15,11 +15,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm ShibeBot by @bosniandoge!\nPowered by aiogram.")
+    await message.reply("Hi!\nI'm ShibeBot by @bosniandoge!\nPowered by aiogram.\nSource Code: https://github.com/bosniandoge/shibebot")
 
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message):
-	await message.reply("To use this bot do:\n /doge - if you want a shibe picture")
+	await message.reply("To use this bot do:\n /doge - if you want a shibe picture \n /cat - if you want a cat picture \n /bird - if you want a bird picture.")
 
 
 @dp.message_handler(commands=['doge'])
@@ -29,9 +29,26 @@ async def cmd_image(message: types.Message):
 	dogeurl = r.content
 	urlsecond = dogeurl[2:-2].decode('utf-8')
 	print(urlsecond)
-	dogeimg = requests.get(urlsecond)
-	print(dogeimg)
 	await bot.send_photo(message.chat.id, types.InputFile.from_url(str(urlsecond)))
+
+@dp.message_handler(commands=['cat'])
+async def cmd_image(message: types.Message):
+	url = 'https://shibe.online/api/cats?count=1&urls=true&httpsUrls=true'
+	r = requests.get(url)
+	birdurl = r.content
+	urlsecond = birdurl[2:-2].decode('utf-8')
+	print(urlsecond)
+	await bot.send_photo(message.chat.id, types.InputFile.from_url(str(urlsecond)))
+
+@dp.message_handler(commands=['bird'])
+async def cmd_image(message: types.Message):
+	url = 'https://shibe.online/api/birds?count=1&urls=true&httpsUrls=true'
+	r = requests.get(url)
+	birdurl = r.content
+	urlsecond = birdurl[2:-2].decode('utf-8')
+	print(urlsecond)
+	await bot.send_photo(message.chat.id, types.InputFile.from_url(str(urlsecond)))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
